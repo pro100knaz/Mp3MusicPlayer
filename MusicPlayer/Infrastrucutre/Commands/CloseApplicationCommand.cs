2fcdcda55eca7633ae324f47764e19ff3544aa8e
MFlEGENDANTON <jffgx@mail.ru>
2024-04-21 13:03:24 +0300

    добавил список команд и команду CloseApplication

---
+++ MusicPlayer/Infrastrucutre/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,21 @@
+﻿using MusicPlayer.Infrastrucutre.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MusicPlayer.Infrastrucutre.Commands
+{
+    class CloseApplicationCommand :Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+
+        public override void Execute(object? parameter)
+        {
+           Application.Current.Shutdown();
+        }
+    }
+}
