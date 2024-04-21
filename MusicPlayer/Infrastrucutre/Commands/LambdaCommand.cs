cb3eb1f27a12fdbe255a3c8bf977f1563d59b079
MFlEGENDANTON <jffgx@mail.ru>
2024-04-21 10:26:12 +0300

    добавил основную инфраструктура шаблона MVVM

---
+++ MusicPlayer/Infrastrucutre/Commands/LambdaCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPlayer.Infrastrucutre.Commands.Base;
+
+namespace MusicPlayer.Infrastrucutre.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _execute;
+
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute ?? throw new ArgumentNullException();
+        }
+        public override bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+    
+        public override void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ MusicPlayer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPlayer.ViewModels.Base;
+
+namespace MusicPlayer.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
