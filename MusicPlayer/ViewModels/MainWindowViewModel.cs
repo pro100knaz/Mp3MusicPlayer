d671e5ccf746c297d204b38edec5f1f7d33a3954
MFlEGENDANTON <jffgx@mail.ru>
2024-04-21 12:25:35 +0300

    Добавил новую тему а также загрузил картинки

--- MusicPlayer/ViewModels/MainWindowViewModel.cs
+++ MusicPlayer/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using MusicPlayer.ViewModels.Base;
 
 namespace MusicPlayer.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-
+        private BitmapImage musicBitmapImage;
+        public MainWindowViewModel()
+        {
+            string path = Environment.CurrentDirectory;
+            //_images.Add(new BitmapImage(new Uri($@"{path}\Images\Deer.jpg")));
+            //C:\Users\jffgx\OneDrive\Рабочий стол\Projects\Mp3Player\MusicPlayer\MusicPlayer\Data\Images\MusicIcon.png
+            musicBitmapImage = new BitmapImage(new Uri($@"{path}\Data\Images\MusicIcon.png"));
+        }
     }
 }
